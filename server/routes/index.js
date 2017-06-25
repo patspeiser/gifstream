153c3a8296b18878c3b63897fdb7c5de07aa4701
patspeiser <pat@socialflow.com>
2017-06-24 20:51:48 -0400

    getting gifs from giphy to front end

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,6 +1,16 @@
 const router = require('express').Router();
+const Util = require('../scripts/index.js');
 module.exports = router;
 
 router.get('/', function(req, res, next){
 	res.send('200 success');
-})
+});
+
+router.get('/trending', function(req, res, next){
+	Util.trending()
+		.then(function(data){
+			res.send(data);
+		})
+		.catch(next);
+});
+
--- server/scripts/index.js
+++ server/scripts/index.js
@@ -1,32 +1,40 @@
 /*get a gif stream*/
+const Promise = require('bluebird');
 var Client = require('node-rest-client').Client;
 var giphy = new Client();
 
-var urlRoot = "http://"
-var host = 'api.giphy.com';
-var path = '/v1/gifs/trending'
 
-var apiKey = ''
-var limit = '25';
-var rating = '';
-
+//lol will work for now
 var config = {
 	urlRoot: "http://",
 	host: "api.giphy.com",
 	path: "/v1/gifs/trending",
 	apiKey: '', //process.env.GIPHY_KEY
-	limit: "/25"
-}
-// going to use this so we can dynamically pass in what we want
-var options = function(){
-
+	limit: "&limit=25",
+	rating: "rating=g"
 }
 
 //URL -> universal resource locator. lols who knew 
+// this is a stupid ass thing to do but i just wanna make it work for now
 function buildUrl(config){
 	var c = config; 
 	return c.urlRoot + c.host + c.path + c.apiKey + c.limit
 }
 
-console.log(buildUrl(config));
-//giphy.get()
+// trending endpoint
+function _trending(){
+	return new Promise(function(resolve, reject){
+		giphy.get(buildUrl(config), function(data, response){
+				if (data){
+					resolve(data);	
+				}  else {
+					reject();
+				}
+		});
+	})
+};
+
+module.exports = {
+	trending: _trending
+}
+
